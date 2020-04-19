c53e99b63fd4d2ed1ea65150635f6f6a142c65ec
prtsh <pratyush.ismu@gmail.com>
2020-04-19 17:58:45 -0400

    Day 18, search in rotated array, binary search

---
+++ DAY18-SearchRotatedArray.cpp
@@ -0,0 +1,45 @@
+/*
+    Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+    
+    (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+    
+    You are given a target value to search. If found in the array return its index, otherwise return -1.
+    
+    You may assume no duplicate exists in the array.
+    
+    Your algorithm's runtime complexity must be in the order of O(log n).
+    
+    Example 1:
+    
+    Input: nums = [4,5,6,7,0,1,2], target = 0
+    Output: 4
+    Example 2:
+    
+    Input: nums = [4,5,6,7,0,1,2], target = 3
+    Output: -1
+*/
+
+
+int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start =0, end = n-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            cout<<mid<<" ";
+            if(nums[mid] == target)
+                return mid;
+            //if both target and mid are on same side
+            if((nums[mid] - nums[n-1])*(target - nums[n-1]) > 0){
+                if(target > nums[mid])
+                    start = mid+1;
+                else
+                    end = mid-1;// -1;
+            }
+            else if(target > nums[n-1]){ //target on the left side
+                end = mid-1;//-1;
+            }else{ //target on the right side, mid on the left side
+                start = mid+1;
+            }
+        }
+        return -1;
+}
