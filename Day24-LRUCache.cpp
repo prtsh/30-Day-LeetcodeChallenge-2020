56e3c794ed354b574730b6bcb9e6090b1ef00b01
prtsh <pratyush.ismu@gmail.com>
2020-04-24 17:13:33 -0400

    Day24, LRU cache design

---
+++ Day24-LRUCache.cpp
@@ -0,0 +1,82 @@
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+
+
+
+
+class LRUCache {
+    int capacity;
+    list<int> cacheline;
+    unordered_map<int, pair<int,list<int>::iterator>> m; // pair<value, position>
+public:
+    LRUCache(int capacity_) {
+        cacheline.resize(capacity_);
+        capacity = capacity_;
+    }
+    
+    int get(int key) {
+        if(m.find(key)!= m.end()){
+            cacheline.erase(m[key].second);
+            cacheline.push_front(key);
+            m[key].second = cacheline.begin();
+            return m[key].first;
+        }
+        else//cache miss
+        {
+            return -1;   
+        }
+    }
+    
+    void put(int key, int value) {
+        //key exists
+        if(m.find(key)!= m.end()){
+            cacheline.erase(m[key].second);
+            cacheline.push_front(key);
+            m[key].second = cacheline.begin();
+            m[key].first = value;
+        }
+        else{//key is not found
+            //evict if the cacheline is full
+            if(cacheline.size() == capacity){
+                int evict = cacheline.back();
+                cacheline.pop_back();
+                m.erase(evict);  
+            }
+            //else insert the key
+            cacheline.push_front(key);
+            //cout<<key<<" ";
+            m[key].second = cacheline.begin();
+            m[key].first = value;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
